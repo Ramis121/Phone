78ab25ef3ce654b3c09a692347b9ea5b09f2f6cf
Рамис Ахметов <akhmetovramis1@gmail.com>
2021-04-27 11:27:07 +0600

    Phone

---
+++ Phone/Phone/Phone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone
+{
+    public class Phone : BaseViewModel
+    {
+        private string title;
+        private string company;
+        private int price;
+
+        public string Title
+        {
+            get => title;
+            set => SetValue(ref title, value);
+        }
+        public string Company
+        {
+            get => company;
+            set => SetValue(ref company, value);
+        }
+        public int Price
+        {
+            get => price;
+            set => SetValue(ref price, value);
+        }
+    }
+}
